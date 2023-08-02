48377dbd1a480800e499560b73b4016ec3952d2c
Ankita Panda <amiankitaexam@gmail.com>
2023-08-03 02:05:11 +0530

    sum of two integers

---
+++ sum_of_two_integers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 = 0, num2 =0, sum = 0;
+    cout << "Enter two numbers";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "sum= "<< sum;
+    return 0;
+}
