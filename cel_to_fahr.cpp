6015f3ef5220f84fe5c70b5989609c4f08e71ca9
Ankita Panda <amiankitaexam@gmail.com>
2023-08-04 00:07:55 +0530

    cel toh fahr

---
+++ cel_to_fahr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int fahr, cel;
+    cout << "Enter the cel temp: ";
+    cin>> cel;
+    fahr = (cel * 9)/5 + 32;
+    cout << " Enter the cel tem: "<< cel;
+    cout << "\nEnter the fahr tem: "<< fahr;
+    return 0;
+}
