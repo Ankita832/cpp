ef9a539a11d5d0c78910c0369c7bf17884e39854
Ankita Panda <amiankitaexam@gmail.com>
2023-08-04 00:13:48 +0530

    perimeter of triangle

---
+++ perimeter_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int side, peri;
+    cout <<"Enter the value of side: ";
+    cin>> side;
+    peri = side*3;
+    cout << "The value od perimeter is :"<< peri;
+    return 0; 
+}
