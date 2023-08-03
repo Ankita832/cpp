3c6ed7aca785af97c1f890fd75f4e444c1261d15
Ankita Panda <amiankitaexam@gmail.com>
2023-08-03 22:44:30 +0530

    area of rec

---
+++ area_of_rec.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length, breadth, area;
+    cout << "Enter the length, breadth of the rectangle";
+    cin>> length >> breadth;
+    area = length * breadth;
+    cout << " The area of a rectangle is : "<< area;
+    return 0;
+}
