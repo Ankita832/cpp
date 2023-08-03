3551d537c50fbb47061fe4acdb200ec2053621e7
Ankita Panda <amiankitaexam@gmail.com>
2023-08-03 23:09:36 +0530

    average of two numbers

---
+++ avg_of_two_num.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, sum, avg;
+    cout << "Enter two numbers\n";
+    cin >> num1>> num2;
+    sum = num1 + num2;
+    cout << " sum is :\n"<< sum;
+    avg = sum /2 ;
+    cout << " Average of two number is :"<< avg;
+    return(0);
+
+}
