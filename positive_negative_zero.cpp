113dcb807dda6c7c384223150301999736525d5f
Ankita Panda <amiankitaexam@gmail.com>
2023-08-03 23:39:23 +0530

    positive negative zero

---
+++ positive_negative_zero.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num =0 ;
+    cout << " enter a number";
+    cin >> num;
+    if (num > 0)
+    cout << "The number is positive";
+     if (num < 0)
+    cout << "The number is negative";
+    else 
+    cout << "Zero";
+    return 0;
+}
