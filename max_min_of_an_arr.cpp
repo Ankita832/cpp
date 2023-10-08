ae2a3424cb90ecf663d7210cf7ea910fad5482ef
Ankita Panda <amiankitaexam@gmail.com>
2023-10-09 00:24:44 +0530

    cpp

--- max_min_of_an_arr.cpp
+++ max_min_of_an_arr.cpp
@@ -13,12 +13,8 @@ int main ()
     {
         if (max < arr[i])
             max = arr[i];
-    }
-    min = arr[0];
-    for (i = 0; i < n; i++)
-    {
-        if (min > arr[i])
-            min = arr[i];
+             else (min > arr[i])
+                min = arr[i];
     }
     cout << "Largest element : " << max;
     cout << "\nSmallest element : " << min;
--- neg_no_one_side.cpp
+++ neg_no_one_side.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void move(int arr[], int n)
+int move(int arr[], int n)
 {
     int j = 0;
     for (int i = 0; i < n; i++)
